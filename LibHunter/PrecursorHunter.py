c35ab68d13dfe43fbfcd77eabf2ad619d5ac4590
Zhixu Ni <zhixu.ni@uni-leipzig.de>
2017-03-12 23:15:38 +0100

    Enable multiprocessing for PrecursorHunter

--- LibHunter/PrecursorHunter.py
+++ LibHunter/PrecursorHunter.py
@@ -15,27 +15,87 @@
 from __future__ import division
 from __future__ import print_function
 
-import gc
+import math
 
 import pandas as pd
-import numpy as np
+from multiprocessing import Pool
 
 from ParallelFunc import ppm_calc_para, ppm_window_para, pr_window_calc_para
+
+
+def find_pr_info(scan_info_df, spectra_pl, lpp_info_groups, all_group_key_lst, sub_group_list, ms1_th, ms1_ppm):
+    core_results_df = pd.DataFrame()
+    for group_key in sub_group_list:
+        if group_key in all_group_key_lst:
+            subgroup_df = lpp_info_groups.get_group(group_key)
+
+            same_mz_se = subgroup_df.iloc[0, :].squeeze()
+            _pr_code = '%f<= MS2_PR_mz <= %f' % (same_mz_se['PR_MZ_LOW'], same_mz_se['PR_MZ_HIGH'])
+
+            _tmp_scan_info_df = scan_info_df.query(_pr_code).copy()
+
+            if _tmp_scan_info_df.shape[0] > 0:
+
+                for idx, row in _tmp_scan_info_df.iterrows():
+                    ms2_pr = row['MS2_PR_mz']
+                    ms2_dda_idx = row['dda_event_idx']
+                    ms2_dda_rank = row['DDA_rank']
+                    ms2_spec_idx = row['spec_index']
+                    ms2_scan_time = row['scan_time']
+                    ms2_scan_number = row['scan_number']
+
+                    _ms1_query_code = 'dda_event_idx == %i and DDA_rank == 0' % ms2_dda_idx
+                    tmp_ms1_info_df = scan_info_df.query(_ms1_query_code).copy()
+
+                    if tmp_ms1_info_df.shape[0] > 0:
+
+                        ms1_spec_idx = tmp_ms1_info_df['spec_index'].tolist()[0]
+
+                        if ms1_spec_idx in spectra_pl.items:
+                            ms1_df = spectra_pl[ms1_spec_idx]
+                            pr_ms1_df = ms1_df.query('i > %f and %f <= mz <= %f' % (ms1_th,
+                                                                                    same_mz_se['MS1_MZ_LOW'],
+                                                                                    same_mz_se['MS1_MZ_HIGH']))
+                            if pr_ms1_df.shape[0] > 0:
+
+                                pr_ms1_df.loc[:, 'ppm'] = ppm_calc_para(pr_ms1_df['mz'].values, same_mz_se['Lib_mz'])
+                                pr_ms1_df.loc[:, 'abs_ppm'] = abs(pr_ms1_df.loc[:, 'ppm'])
+                                pr_info_df = pr_ms1_df.query('abs_ppm <= %i' % ms1_ppm).copy()
+                                if pr_info_df.shape[0] > 0:
+                                    pr_info_df.sort_values(by='i', ascending=False, inplace=True)
+                                    pr_info_df.reset_index(drop=True, inplace=True)
+                                    _ms1_mz = pr_info_df['mz'].tolist()[0]
+                                    _ppm = pr_info_df['ppm'].tolist()[0]
+                                    len_df = subgroup_df.shape[0]
+                                    subgroup_df.loc[:, 'MS1_obs_mz'] = [_ms1_mz] * len_df
+                                    subgroup_df.loc[:, 'dda_event_idx'] = [ms2_dda_idx] * len_df
+                                    subgroup_df.loc[:, 'spec_index'] = [ms2_spec_idx] * len_df
+                                    subgroup_df.loc[:, 'scan_time'] = [ms2_scan_time] * len_df
+                                    subgroup_df.loc[:, 'DDA_rank'] = [ms2_dda_rank] * len_df
+                                    subgroup_df.loc[:, 'scan_number'] = [ms2_scan_number] * len_df
+                                    subgroup_df.loc[:, 'MS2_PR_mz'] = [ms2_pr] * len_df
+                                    subgroup_df.loc[:, 'MS1_XIC_mz'] = [round(_ms1_mz, 4)] * len_df
+                                    subgroup_df.loc[:, 'ppm'] = [_ppm] * len_df
+                                    subgroup_df.loc[:, 'abs_ppm'] = [abs(_ppm)] * len_df
+
+                                    core_results_df = core_results_df.append(subgroup_df)
+
+    return core_results_df
 
 
 class PrecursorHunter(object):
     def __init__(self, lpp_info_df, param_dct):
         self.lpp_info_df = lpp_info_df
         self.param_dct = param_dct
-        gc.disable()
 
-    def get_matched_pr(self, scan_info_df, spectra_pl):
+    def get_matched_pr(self, scan_info_df, spectra_pl, core_num=4):
 
         print('Start match!!!!')
 
         pr_window = self.param_dct['pr_window']
-        ms1_th = self.param_dct['ms_th']
+
         ms1_ppm = self.param_dct['ms_ppm']
+        ms1_th = self.param_dct['ms_th']
 
         pl_class = self.param_dct['lipid_type']
         charge_mode = self.param_dct['charge_mode']
@@ -82,61 +142,32 @@ class PrecursorHunter(object):
             self.lpp_info_df.loc[:, 'Ion'] = '[M-H]-'
             self.lpp_info_df.loc[:, 'Lib_mz'] = self.lpp_info_df.loc[:, '[M-H]-_MZ']
 
-        for _n, _subgroup_df in self.lpp_info_df.groupby(['Lib_mz', 'Formula']):
-            _samemz_se = _subgroup_df.iloc[0, :].squeeze()
+        lpp_info_groups = self.lpp_info_df.groupby(['Lib_mz', 'Formula'])
+        all_group_key_lst = lpp_info_groups.groups.keys()
+        sub_len = int(math.ceil(len(all_group_key_lst) / core_num))
+        core_key_list = map(None,  * (iter(lpp_info_groups.groups.keys()),) * sub_len)
+        print(core_key_list)
+        # Start multiprocessing
+        print('!!!!!! Start multiprocessing ==> ==> ==> Number of Cores: %i' % core_num)
+        parallel_pool = Pool()
+        pr_info_results_lst = []
+        for core_list in core_key_list:
+            pr_info_result = parallel_pool.apply_async(find_pr_info, args=(scan_info_df, spectra_pl, lpp_info_groups,
+                                                                           all_group_key_lst, core_list,
+                                                                           ms1_th, ms1_ppm))
+            pr_info_results_lst.append(pr_info_result)
 
-            _pr_code = '%f<= MS2_PR_mz <= %f' % (_samemz_se['PR_MZ_LOW'], _samemz_se['PR_MZ_HIGH'])
+        parallel_pool.close()
+        parallel_pool.join()
 
-            _tmp_scan_info_df = scan_info_df.query(_pr_code)
+        for pr_info_result in pr_info_results_lst:
+            sub_df = pr_info_result.get()
+            if sub_df.shape[0] > 0:
+                ms1_obs_pr_df = ms1_obs_pr_df.append(sub_df)
 
-            if _tmp_scan_info_df.shape[0] > 0:
+        # End multiprocessing
 
-                for idx, row in _tmp_scan_info_df.iterrows():
-
-                    ms2_pr = row['MS2_PR_mz']
-                    ms2_dda_idx = row['dda_event_idx']
-                    ms2_dda_rank = row['DDA_rank']
-                    ms2_spec_idx = row['spec_index']
-                    ms2_scan_time = row['scan_time']
-                    ms2_scan_number = row['scan_number']
-
-                    _ms1_query_code = 'dda_event_idx == %i and DDA_rank == 0' % ms2_dda_idx
-                    tmp_ms1_info_df = scan_info_df.query(_ms1_query_code)
-
-                    if tmp_ms1_info_df.shape[0] > 0:
-
-                        ms1_spec_idx = tmp_ms1_info_df['spec_index'].tolist()[0]
-
-                        if ms1_spec_idx in spectra_pl.items:
-                            ms1_df = spectra_pl[ms1_spec_idx]
-                            pr_ms1_df = ms1_df.query('i > %f and %f <= mz <= %f' % (ms1_th, _samemz_se['MS1_MZ_LOW'],
-                                                                                    _samemz_se['MS1_MZ_HIGH']))
-                            if pr_ms1_df.shape[0] > 0:
-
-                                # find the best ms1 pr with min abs_ppm
-                                # pr_ms1_df.loc[:, 'ppm'] = (1e6 * (pr_ms1_df.loc[:, 'mz'] - _samemz_se['Lib_mz']) /
-                                #                            _samemz_se['Lib_mz'])
-                                pr_ms1_df.loc[:, 'ppm'] = ppm_calc_para(pr_ms1_df['mz'].values, _samemz_se['Lib_mz'])
-                                pr_ms1_df.loc[:, 'abs_ppm'] = abs(pr_ms1_df.loc[:, 'ppm'])
-                                pr_info_df = pr_ms1_df.query('abs_ppm <= %i' % ms1_ppm)
-                                if pr_info_df.shape[0] > 0:
-                                    pr_info_df = pr_info_df.sort_values(by='i', ascending=False).reset_index(drop=True)
-                                    _ms1_mz = pr_info_df['mz'].tolist()[0]
-                                    _ppm = pr_info_df['ppm'].tolist()[0]
-                                    _subgroup_df.loc[:, 'MS1_obs_mz'] = _ms1_mz
-                                    _subgroup_df.loc[:, 'dda_event_idx'] = ms2_dda_idx
-                                    _subgroup_df.loc[:, 'spec_index'] = ms2_spec_idx
-                                    _subgroup_df.loc[:, 'scan_time'] = ms2_scan_time
-                                    _subgroup_df.loc[:, 'DDA_rank'] = ms2_dda_rank
-                                    _subgroup_df.loc[:, 'scan_number'] = ms2_scan_number
-                                    _subgroup_df.loc[:, 'MS2_PR_mz'] = ms2_pr
-                                    _subgroup_df.loc[:, 'MS1_XIC_mz'] = round(_ms1_mz, 4)
-                                    _subgroup_df.loc[:, 'ppm'] = _ppm
-                                    _subgroup_df.loc[:, 'abs_ppm'] = abs(_ppm)
-                                    # print(_subgroup_df[['Abbreviation', 'spec_index', 'MS2_PR_mz', 'MS1_obs_mz']])
-                                    ms1_obs_pr_df = ms1_obs_pr_df.append(_subgroup_df)
-
-        # print('ms1_obs_pr_df.shape', ms1_obs_pr_df.shape)
+        print('ms1_obs_pr_df.shape', ms1_obs_pr_df.shape)
         if ms1_obs_pr_df.shape[0] > 0:
             ms1_obs_pr_df = ms1_obs_pr_df.sort_values(by=['Lib_mz', 'abs_ppm'], ascending=[True, True])
             ms1_obs_pr_df = ms1_obs_pr_df.reset_index(drop=True)
